8db646afd55fa5fc2eaaafa81edc99e78b223953
xoz0359 <k84890690@gmail.com>
2025-05-26 14:07:42 +0900

    init

---
+++ src/main/java/io/cavia/homenet/HomenetApplication.java
@@ -0,0 +1,13 @@
+package io.cavia.homenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cavia/homenet/HomenetApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cavia.homenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
