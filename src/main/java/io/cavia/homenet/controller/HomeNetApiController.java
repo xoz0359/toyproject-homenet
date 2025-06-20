3023f15dad912ae8e0302884103ceafb53873601
xoz0359 <k84890690@gmail.com>
2025-06-20 14:18:23 +0900

    fix: 삭제 요청 DeleteMapping으로 변경

--- src/main/java/io/cavia/homenet/controller/HomeNetApiController.java
+++ src/main/java/io/cavia/homenet/controller/HomeNetApiController.java
@@ -10,10 +10,7 @@ import io.cavia.homenet.service.SummaryStocksDataService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -87,7 +84,7 @@ public class HomeNetApiController {
         }
     }
 
-    @GetMapping("/stocks/delete/{id}")
+    @DeleteMapping("/stocks/delete/{id}")
     public ResponseEntity<?> deleteStocks(@PathVariable int id) {
         try {
             summaryStocksDataService.deleteStocks(id);
