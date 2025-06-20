2366841f44c32068ab574f41dd51f528cb7cedbc
xoz0359 <k84890690@gmail.com>
2025-06-20 14:21:53 +0900

    fix: 자동완성으로 생성된 서비스의 abstract 태그 삭제

--- src/main/java/io/cavia/homenet/service/SummaryStocksDataServiceImpl.java
+++ src/main/java/io/cavia/homenet/service/SummaryStocksDataServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public abstract class SummaryStocksDataServiceImpl implements SummaryStocksDataService {
+public class SummaryStocksDataServiceImpl implements SummaryStocksDataService {
 
     private final StocksDefaltRepository stockDefaltRepository;
     private final TradesDefaltRepository tradesDefaltRepository;
